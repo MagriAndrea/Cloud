9f8e018bb2b54d29897e26e31692207bebd68897
MagriAndrea <magriandrea.03@gmail.com>
2024-04-23 23:00:03 +0200

    aggiunto mantine e cambio tema

--- ESAME-REACT/client/src/App.jsx
+++ ESAME-REACT/client/src/App.jsx
@@ -1,22 +1,46 @@
-import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from "react-router-dom"
 import Layout from "./layout/layout"
 import ClientList from "./pages/ClientList"
+import ClientDetail from "./pages/ClientDetail"
+
+import classes from "./App.module.css"
+import '@mantine/core/styles.css';
+import { MantineProvider, createTheme, rem } from '@mantine/core';
+
+const theme = createTheme({
+
+  shadows: {
+    md: '1px 1px 3px rgba(0, 0, 0, .25)',
+    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+  },
+
+  headings: {
+    fontFamily: 'Roboto, sans-serif',
+    sizes: {
+      h1: { fontSize: rem(36) },
+    },
+  },
+  autoContrast: true,
+  luminanceThreshold: 0.47,
+  activeClassName: classes.active,
+});
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={Layout}>
-      <Route path="/" element={<Navigate to="/clientlist" />} /> {/*Faccio in modo che la route / vada sulla route /clientlist */}
-      <Route path="/clientlist" element={<ClientList/>}/>
-      <Route path="/clientdetail" element={<ClientDetail/>}/>
+    <Route path="/" element={<Layout />}>
+      <Route path="/clientlist" element={<ClientList />} index />
+      <Route path="/clientdetail" element={<ClientDetail />} />
+      <Route path="/booklist" element={<ClientList />} index />
+      <Route path="/bookdetail" element={<ClientDetail />} />
     </Route>
   )
 )
 
 function App() {
   return (
-    <>
-      <RouterProvider router={router}/>
-    </>
+    <MantineProvider theme={theme} defaultColorScheme="light">
+      <RouterProvider router={router} />
+    </MantineProvider>
   )
 }
 
--- ESAME-REACT/client/src/layout/layout.jsx
+++ ESAME-REACT/client/src/layout/layout.jsx
@@ -1,23 +1,53 @@
-import { Link, Outlet } from 'react-router-dom';
+import { AppShell, Burger, Group, UnstyledButton } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import classes from './Layout.module.css';
+import ToggleColorScheme from '../components/ToggleColorScheme';
 
-const Layout = () => {
+export default function Layout() {
+  const [opened, { toggle }] = useDisclosure();
+
+  const routes = [
+    { name: "Lista Clienti", path: "/clientlist" },
+    { name: "Dettaglio Clienti", path: "/clientdetail" },
+    { name: "Lista Libri", path: "/booklist" },
+    { name: "Dettaglio Libri", path: "/bookdetail" },
+  ]
+
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/books">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <Outlet />
-      </div>
-    </div>
-  );
-};
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { desktop: true, mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Group justify="space-between" style={{ flex: 1 }}>
+            Biblioteca
+            <Group ml="xl" gap={6} visibleFrom="sm">
+              <UnstyledButton className={classes.control}>Home</UnstyledButton>
+              <UnstyledButton className={classes.control}>Blog</UnstyledButton>
+              <UnstyledButton className={classes.control}>Contacts</UnstyledButton>
+              <UnstyledButton className={classes.control}>Support</UnstyledButton>
+            </Group>
+          </Group>
+          <Group ml="xl" gap={0}>
+            <ToggleColorScheme />
+          </Group>
+        </Group>
+      </AppShell.Header>
 
-export default Layout;
+      <AppShell.Navbar py="md" px={4}>
+        <UnstyledButton className={classes.control}>Home</UnstyledButton>
+        <UnstyledButton className={classes.control}>Blog</UnstyledButton>
+        <UnstyledButton className={classes.control}>Contacts</UnstyledButton>
+        <UnstyledButton className={classes.control}>Support</UnstyledButton>
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        Navbar is only visible on mobile, links that are rendered in the header on desktop are
+        hidden on mobile in header and rendered in navbar instead.
+      </AppShell.Main>
+    </AppShell>
+  );
+}
